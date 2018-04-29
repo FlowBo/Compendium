2edbf8d546c125d083ca94f51a81e577d0c2647c
Florian Born <hello@florianborn.com>
2018-04-30 00:12:57 +0200

    updated push routine

--- publish.sh
+++ publish.sh
@@ -1,21 +1,21 @@
 # install the plugins and build the static site
-gitbook install && gitbook build
+# gitbook install && gitbook build
 
 # checkout to the gh-pages branch
-git checkout master
+# git checkout master
 
 # pull the latest updates
-git pull Compendium master --rebase
+# git pull Compendium master --rebase
 
 # copy the static site files into the current directory.
-cp -R _book/* .
+# cp -R _book/* .
 
 # remove 'node_modules' and '_book' directory
-git clean -fx node_modules
-git clean -fx _book
+# git clean -fx node_modules
+# git clean -fx _book
 
 # add all files
-git add .
+# git add .
 
 # commit
 git commit -a -m "Update docs"
@@ -24,4 +24,4 @@ git commit -a -m "Update docs"
 git push Compendium master
 
 # checkout to the master branch
-git checkout master
+# git checkout master
